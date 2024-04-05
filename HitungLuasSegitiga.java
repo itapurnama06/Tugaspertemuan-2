d7fe2271b3367eb1fc3cc457bc5e236149ee72b7
itapurnama06 <127809157+itapurnama06@users.noreply.github.com>
2024-04-05 12:52:16 +0800

    Add files via upload

---
+++ HitungLuasSegitiga.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author LAB F
+ */
+public class HitungLuasSegitiga {
+     public static void main(String[] args) {
+        // Nilai alas
+        double alas = 40; // dalam satuan cm
+        
+        // Nilai tinggi
+        double tinggi = 60; // dalam satuan cm
+        
+        // Menghitung luas segitiga
+        double luas = (alas * tinggi) / 2;
+        
+        // Menampilkan hasil
+        System.out.println("Luas segitiga dengan alas 40 cm dan tinggi 60 cm adalah: " + luas + " cm^2");
+    }
+
+}
